9d6eb21e62c449881d880828aa0273f060434e86
arcaneCheco <sergio.azizi.14@gmail.com>
2022-10-27 15:40:11 +0100

    feat: update about description

--- src/viewManagers/about/components/Screen.ts
+++ src/viewManagers/about/components/Screen.ts
@@ -13,11 +13,9 @@ export class Screen implements _AboutScreen {
     padding: { x: 0.2, y: 0.2 },
     font: this.world.resources.fonts.anironRegular,
     text: `
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the industry*s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\n\`\n
-        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\`\n
-        Here is another paragraph. This probably won't be immediately visible but that is fine.\n\`\n
-        Also, look this, Heyyy, hoo.
+        I'm a full-stack developer currently employed at London-based media/tech company IMG Arena, where I'm building products that aim to bolster fan engagement for live sporting events through instant data feeds, 3D data visualisation and interactivity.
+        I love writing shaders and create 3D and interactive websites.\n\~\n
+        If you are looking for freelance developers or want to collaborate on a projects feel free to reach out!
         `,
   });
   aspect = 2;
@@ -28,7 +26,7 @@ export class Screen implements _AboutScreen {
       uTextMap: this.textTexture.texture,
       uMouse: { value: new Vector2() },
       uAspect: { value: this.aspect },
-      uDistortion: { value: 0.5 },
+      uDistortion: { value: 0.35 },
       uInfluence: { value: 0.25 },
       uTest: { value: 0.48 },
       uProgress: { value: 0 },
@@ -56,6 +54,6 @@ export class Screen implements _AboutScreen {
   onResize(sizes) {
     this.mesh.scale.set(sizes.scaleX, sizes.scaleY, 1);
     this.mesh.position.y = sizes.posY;
-    this.material.uniforms.uAspect.value = sizes.aspect;
+    // this.material.uniforms.uAspect.value = sizes.aspect;
   }
 }
